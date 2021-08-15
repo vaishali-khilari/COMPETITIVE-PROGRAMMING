e288139e92aea834d0d43d9b7849b03b6031af14
vaishali-khilari <69888100+vaishali-khilari@users.noreply.github.com>
2021-08-15 22:33:45 +0530

    Create number_of_1_bits.py

---
+++ leetcode/number_of_1_bits.py
@@ -0,0 +1,35 @@
+title:nuber of one bits:
+
+
+Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
+
+Note:
+
+Note that in some languages, such as Java, there is no unsigned integer type. In this case, the input will be given as a signed integer type. It should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
+In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 3, the input represents the signed integer. -3.
+ 
+
+Example 1:
+
+Input: n = 00000000000000000000000000001011
+Output: 3
+Explanation: The input binary string 00000000000000000000000000001011 has a total of three '1' bits.
+Example 2:
+
+Input: n = 00000000000000000000000010000000
+Output: 1
+Explanation: The input binary string 00000000000000000000000010000000 has a total of one '1' bit.
+
+code:
+  
+  class Solution:
+    def hammingWeight(self, n: int) -> int:
+        
+        c = 0
+        while n:
+            n &= n - 1 #n-1 will remove every 1 at a time
+            c += 1
+        return c
+
+
+  
