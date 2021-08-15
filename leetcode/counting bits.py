f0c93031950f475bc85cd0cceb6e3d753a2d6edd
vaishali-khilari <69888100+vaishali-khilari@users.noreply.github.com>
2021-08-15 22:21:15 +0530

    Create counting bits.py

---
+++ leetcode/counting bits.py
@@ -0,0 +1,43 @@
+#counting bits
+
+
+# Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+
+
+# Example 1:
+
+# Input: n = 2
+# Output: [0,1,1]
+# Explanation:
+# 0 --> 0
+# 1 --> 1
+# 2 --> 10
+# Example 2:
+
+# Input: n = 5
+# Output: [0,1,1,2,1,2]
+# Explanation:
+# 0 --> 0
+# 1 --> 1
+# 2 --> 10
+# 3 --> 11
+# 4 --> 100
+# 5 --> 101
+
+
+code:
+  class Solution:
+    def countBits(self, n: int) -> List[int]:
+        counter=[0]
+        for i in range(1,n+1):
+            counter.append(counter[i//2]+i%2)
+        return counter
+        
+
+
+
+
+
+
+
+
